dd6c81378547e042c3abd85c91be52d68dfb0347
Pavel <paleon@mail.ru>
2024-02-14 14:51:22 +0300

    4. Сравнение моделей. Метод equals [#174893 #480821]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -2,34 +2,57 @@ package ru.job4j.oop;
 
 public class Calculator {
 
-    private static int x = 5;
+    public static class Multiple {
 
-    public static int sum(int y) {
-        return x + y;
+        private final int result;
+
+        public Multiple(int number) {
+            result = number;
+        }
+
+        public int getResult() {
+            return result;
+        }
     }
 
-    public int multiply(int a) {
-        return x * a;
-    }
-
-    public static int minus(int a) {
-        return a - x;
-    }
-
-    public int divide(int a) {
-        return a / x;
-    }
-
-    public int sumAllOperation(int a) {
-        return sum(a) + multiply(a) + minus(a) + divide(a);
+    public static Multiple getMultiple(int value) {
+        int result = value * value;
+        return  new Multiple(result);
     }
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        sum(10);
-        calculator.multiply(5);
-        Calculator.minus(8);
-        calculator.divide(4);
-        calculator.sumAllOperation(15);
+        Calculator.Multiple multiple = Calculator.getMultiple(3);
+        System.out.println("Квадрат числа равен " + multiple.getResult());
     }
+
+//    private static int x = 5;
+//
+//    public static int sum(int y) {
+//        return x + y;
+//    }
+//
+//    public int multiply(int a) {
+//        return x * a;
+//    }
+//
+//    public static int minus(int a) {
+//        return a - x;
+//    }
+//
+//    public int divide(int a) {
+//        return a / x;
+//    }
+//
+//    public int sumAllOperation(int a) {
+//        return sum(a) + multiply(a) + minus(a) + divide(a);
+//    }
+//
+//    public static void main(String[] args) {
+//        Calculator calculator = new Calculator();
+//        sum(10);
+//        calculator.multiply(5);
+//        Calculator.minus(8);
+//        calculator.divide(4);
+//        calculator.sumAllOperation(15);
+//    }
 }
