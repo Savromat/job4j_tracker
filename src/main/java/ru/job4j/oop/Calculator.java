0a25bf88d568dd2ab54518dc72f29af357a491f1
Pavel <paleon@mail.ru>
2024-02-17 13:52:24 +0300

    4. Сравнение моделей. Метод equals [#174893 #480821]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -24,35 +24,4 @@ public class Calculator {
         Calculator.Multiple multiple = Calculator.getMultiple(3);
         System.out.println("Квадрат числа равен " + multiple.getResult());
     }
-
-//    private static int x = 5;
-//
-//    public static int sum(int y) {
-//        return x + y;
-//    }
-//
-//    public int multiply(int a) {
-//        return x * a;
-//    }
-//
-//    public static int minus(int a) {
-//        return a - x;
-//    }
-//
-//    public int divide(int a) {
-//        return a / x;
-//    }
-//
-//    public int sumAllOperation(int a) {
-//        return sum(a) + multiply(a) + minus(a) + divide(a);
-//    }
-//
-//    public static void main(String[] args) {
-//        Calculator calculator = new Calculator();
-//        sum(10);
-//        calculator.multiply(5);
-//        Calculator.minus(8);
-//        calculator.divide(4);
-//        calculator.sumAllOperation(15);
-//    }
 }
