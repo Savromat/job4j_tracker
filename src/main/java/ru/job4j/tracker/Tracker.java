5bb78490a185d67fbb0ae470cb236940c0410366
Pavel <paleon@mail.ru>
2024-06-15 22:04:40 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #500465]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,9 +56,8 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            System.out.println("Ошибка. Был введен не существующий id");
-        } else {
+        boolean result = index != -1;
+        if (result) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
