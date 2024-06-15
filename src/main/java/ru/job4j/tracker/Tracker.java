3a633ec46dafe9c8ee34a463f2271f674da8650c
Pavel <paleon@mail.ru>
2024-06-15 21:53:55 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #482508]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -38,7 +38,7 @@ public class Tracker {
         boolean result = index != -1;
         if (result) {
             items[index] = item;
-            items[index].setId(id);
+            item.setId(id);
         }
         return result;
     }
