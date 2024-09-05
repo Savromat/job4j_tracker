97d2a0d4be4676c84634a7e9d09e192b03e8be2a
Pavel <paleon@mail.ru>
2024-09-05 15:35:31 +0300

    1. Singleton [#94606 #511881]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -4,7 +4,19 @@ import java.util.Arrays;
 
 public class SingleTracker {
 
+    private static SingleTracker instance = null;
+
     private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
